710b504aca67ffed1a9c16616a7d4c1f14d5fb5f
ula1990 <uladzislau.daratsiuk@gmail.com>
2018-04-13 21:50:16 +0200

    added new screens

---
+++ SwedLon/ViewControllers/AboutVC.swift
@@ -0,0 +1,18 @@
+//
+//  AboutVC.swift
+//  SwedLon
+//
+//  Created by Uladzislau Daratsiuk on 13/04/2018.
+//  Copyright © 2018 Uladzislau Daratsiuk. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
---
+++ SwedLon/ViewControllers/HistoryVC.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryVC.swift
+//  SwedLon
+//
+//  Created by Uladzislau Daratsiuk on 13/04/2018.
+//  Copyright © 2018 Uladzislau Daratsiuk. All rights reserved.
+//
+
+import UIKit
+
+class HistoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
